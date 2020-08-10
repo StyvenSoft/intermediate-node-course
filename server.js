fdf69e4fdba0d78e710827dabad59017fb145c13
Steveen <steveen.echeverri@gmail.com>
2020-08-09 23:10:18 -0500

    Using spread syntax and refactor completed

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
 
-const User=require('./models/User');
+const User = require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 sendResponse = (res, err, data) => {
-  if (err){
+  if (err) {
     res.json({
       success: false,
       message: err
     })
-  } else if (!data){
+  } else if (!data) {
     res.json({
       success: false,
       message: "Not Found!"
@@ -33,46 +33,38 @@ sendResponse = (res, err, data) => {
 }
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users', (req, res) => {
   // User.create()
   User.create(
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
-    (err,data)=>{sendResponse(res,err,data)}
+    { ...req.body.newData },
+    (err, data) => { sendResponse(res, err, data) }
   )
 })
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-  User.findById(
-    req.params.id,
-    (err, data)=> {sendResponse(res,err,data)}   
-  )
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-  User.findByIdAndUpdate(
-    req.params.id,
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
-    {new:true},
-    (err,data)=>{sendResponse(res,err,data)}
-  )
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-  User.findByIdAndDelete(
-    req.params.id,
-    (err, data) => {sendResponse(res,err,data)}
-  )
-})
+  // READ
+  .get((req, res) => {
+    // User.findById()
+    User.findById(
+      req.params.id,
+      (err, data) => { sendResponse(res, err, data) }
+    )
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+    User.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body.newData },
+      { new: true },
+      (err, data) => { sendResponse(res, err, data) }
+    )
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+    User.findByIdAndDelete(
+      req.params.id,
+      (err, data) => { sendResponse(res, err, data) }
+    )
+  })
